e51edb8ddbbf3a213ff6ed21757a85443e5b0e4d
Jake Nead <jakenead@gmail.com>
2022-12-02 19:41:36 -0800

    Given two integers find the sum of all the integers between and including them.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function findShort(s){
-  let words = s.split(' ')
-  let shortest = words[0].length
-  for (let i=0; i<words.length; i++) {
-    if (words[i].length<shortest) {
-      shortest = words[i].length
+// Given two integers find the sum of all the integers between and including them. If they are the same return one of them. 
+
+function getSum( a,b ) {
+  let min = Math.min(a, b)
+  let max = Math.max(a, b)
+  return (max-min + 1)*(min + max) / 2
     }
-  }
-  return shortest
-}
+  
 
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
-console.log(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
-console.log(findShort("Let's travel abroad shall we"), 2);
+console.log(getSum(3,3),3)
+console.log(getSum(0,-1),-1)
+console.log(getSum(0,1),1)
+console.log(getSum(-2,7),25)
