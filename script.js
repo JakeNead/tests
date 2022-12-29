d3bda7752f675ca6d4f120feff3a384ab27dda0a
Jake Nead <jakenead@gmail.com>
2022-12-28 21:47:06 -0800

    Reverse each word and preserve the spaces

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const price = document.querySelector("#price");
-const output = document.querySelector(".price-output");
+function reverseWords (str) {
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+}
 
-output.textContent = price.value;
-
-price.addEventListener("input", () => {
-  output.textContent = price.value
-});
-
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.') === 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+console.log(reverseWords('apple'), ' = elppa');
+console.log(reverseWords('a b c d'), ' = a b c d');
+console.log(reverseWords('double  spaced  words'), ' = elbuod  decaps  sdrow');
