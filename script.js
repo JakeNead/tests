bb153377f6f66caae62bb4805cd168a06e6f7957
Jake Nead <jakenead@gmail.com>
2023-01-21 16:32:11 -0800

    My first object constructor

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function findOdd(a) {
-  let count = 0;
-  for (let i = 0; i <= a.length; i++) {
-    count = 0;
-    for (let j=0; j<= a.length; j++) {
-      if (a[i] === a[j])
-        count += 1;
-        }
-    if (count % 2 !== 0)
-      return a[i]
-  }
+function Book (title, author, pages, hasRead) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.hasRead = hasRead
+  this.info = function () {
+     return `${title} by ${author}, ${pages} pages, ${hasRead ? 'previously read' : 'has not been read yet'}`
+    }
 }
-console.log(`${findOdd([7])} = 7`)
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '304', true)
 
-console.log(`${findOdd([0])} = 0`)
-console.log(`${findOdd([0,1,0,1,0])} = 0`)
-console.log(`${findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])} = 4`)
+
+console.log(theHobbit.info())
