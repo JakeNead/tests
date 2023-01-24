6a0ef9338d47433147a419170f9c87723eace28e
Jake Nead <jakenead@gmail.com>
2023-01-24 09:54:42 -0800

    Given a string return the string with alternating upper/lower case characters

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function order(words){
-return words.split(' ').sort((a,b) => (a.split('').filter(Number)) - (b.split('').filter(Number))).join(' ')
-
+function spongeBobCase (str) {
+  result = ''
+  for (let i = 0; i < str.length; i++) {
+      if (i % 2 !== 0) {
+        result += str[i]
+    } else {
+        result += String(str[i]).toUpperCase()
+    }
+  }
+  return result
 }
 
-// return words.split(' ').sort((x,y) => x.split('').filter(x => typeof(x)), y.split('').filter(y => typeof(y)))
 
-
-console.log(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
-console.log(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
-console.log(order(""), "empty input should return empty string" )
-
+console.log(spongeBobCase('Elephant is very large'))
+console.log(spongeBobCase(''))
+console.log(spongeBobCase('hello'))
+console.log(spongeBobCase('hello?????'))
