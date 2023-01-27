5f7d33449eccbdaab8f776ae0572c4062ed609e5
Jake Nead <jakenead@gmail.com>
2023-01-26 16:54:26 -0800

    Practice using an object constructor, this and bind()

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-function spongeBobCase (str) {
-  result = ''
-  for (let i = 0; i < str.length; i++) {
-      if (i % 2 !== 0) {
-        result += str[i]
-    } else {
-        result += String(str[i]).toUpperCase()
-    }
-  }
-  return result
+function Dog(firstName, size, rating) {
+  this.firstName = firstName;
+  this.size = size;
+  this.rating = rating;
 }
 
+function sentence () {
+  return `${this.firstName} has a ${this.size} bark`
+}
 
-console.log(spongeBobCase('Elephant is very large'))
-console.log(spongeBobCase(''))
-console.log(spongeBobCase('hello'))
-console.log(spongeBobCase('hello?????'))
+const marty = Object.create(Dog);
+
+marty.firstName = 'Marty';
+marty.size = 'big';
+marty.rating = '13/10';
+marty.sentence = sentence();
+
+const sentenceBoundToMarty = sentence.bind(marty)
+
+console.log(sentenceBoundToMarty()); // Marty has a big bark
+
+
+
+// const obj = {
+//   sound: "beep beep"
+// }
+
+// function talk() {
+//     console.log(this.sound)
+// }
+// const boromir = {
+//   sound: 'One does not simply walk into mordor'
+// }
+// const talkBoundToBoromier = talk.bind(boromir)
+
+// talkBoundToBoromier()
+// talk()
