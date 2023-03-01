a0ffd36f507eae1ad8d9cc68138165eb828186a0
Jake Nead <jakenead@gmail.com>
2023-02-28 16:33:07 -0800

    Remove duplicated function

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-// Code
-// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
-// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+// For example:
 
-// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
-// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
 
-// The string has a length greater or equal to one and contains only letters from ato z.
-
-// wildly verbose first attempt
-function printerError(s) {
-    let errors = 0;
-    const x = s.split('')
-    let i = 0;
-    while (i < x.length) {
-       if (x[i]==='n'||x[i]==='o'||x[i]==='p'||x[i]==='q'||x[i]==='r'||x[i]==='s'||x[i]==='t'||x[i]==='u'||x[i]==='v'||x[i]==='w'||x[i]==='x'||x[i]==='y'||x[i]==='z')
-      errors ++;
-     i++
-      }
-    return `${errors}/${s.length}`
- }
- 
- // refined second attempt
- function betterPrinterError(s) {
-    // your code
-    let errorCount = 0;
-    for(let i = 0; i < s.length; i++) {
-      if (s[i] > "m") {
-        errorCount++;
+function uniqueInOrder(it) {
+    const result = []
+    let last
+    
+    for (let i = 0; i < it.length; i++) {
+      if (it[i] !== last) {
+        result.push(last = it[i])
       }
     }
-    return `${errorCount}/${s.length}`;
-}
+    
+    return result
+  }
+  console.log(uniqueInOrder([]))
+  console.log(uniqueInOrder(['a','b','b','c', 'd', 'a', 'b']))
+  console.log(uniqueInOrder([1,2,3,3,4,4,5]))
+  console.log(uniqueInOrder('AAAABBBBBCCCCCAB'))
 
-console.log(printerError("aaabbbbhaijjjm") === '0/14')
-console.log(printerError("aaaxbbbbyyhwawiwjjjwwm")=== '8/22')
-
-console.log(betterPrinterError("aaabbbbhaijjjm") === '0/14')
-console.log(betterPrinterError("aaaxbbbbyyhwawiwjjjwwm")=== '8/22')
+  
