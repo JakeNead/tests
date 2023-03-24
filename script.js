6a8d462a57153d299ddbb00a94f4d7dbde3d9104
Jake Nead <jakenead@gmail.com>
2023-03-23 20:48:13 -0700

    Simple function to track number of people on a bus

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
-
-// For example:
-
-// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+// simple Codewars problem solving to keep sane in all the Webpack tutorials I'm swimming in!
 
 
-function uniqueInOrder(it) {
-    const result = []
-    let last
-    
-    for (let i = 0; i < it.length; i++) {
-      if (it[i] !== last) {
-        result.push(last = it[i])
-      }
-    }
-    
-    return result
+
+
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+
+
+const number = function(busStops){
+	let n = 0;
+	for (let i=0; i<busStops.length; i++) {
+	  n += busStops[i][0]
+	  n -= busStops[i][1]
+	}
+	return n
   }
-  console.log(uniqueInOrder([]))
-  console.log(uniqueInOrder(['a','b','b','c', 'd', 'a', 'b']))
-  console.log(uniqueInOrder([1,2,3,3,4,4,5]))
-  console.log(uniqueInOrder('AAAABBBBBCCCCCAB'))
 
-  
+  console.log(number([[10,0],[3,5],[5,8]]), 5)
+  console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]), 17)
+  console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]),21)
+
